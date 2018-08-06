d97d4a899ca2027f6fbd2d38be4b2940acda3419
omid saedi <omidsaedii@outlook.com>
2018-08-06 05:56:18 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,6 +1,6 @@
 var index = require("./index.js")
 
-if (index.Name === "Susan") {
+if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
     console.log("Expected: Susan, Received: "+index.Name)
